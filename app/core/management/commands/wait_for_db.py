60045ef6a7c1a4a17db3bdf343f96ec87372cfba
Gabriel Gitonga <gabworks51@gmail.com>
2019-12-30 01:40:22 +0300

    fixed flake8 linting issuess

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,6 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
-
     def handle(self, *args, **options):
         """Handle the command"""
         self.stdout.write('Waiting for database...')
@@ -18,5 +17,4 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
