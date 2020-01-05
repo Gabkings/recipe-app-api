d061076603197d41eb3c94bf4be9823f29c61a6d
Gabriel Gitonga <gabworks51@gmail.com>
2020-01-05 05:24:23 +0300

    added an image upload field to the recipe's model

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,\
     BaseUserManager, PermissionsMixin
 from django.conf import settings
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -58,7 +68,7 @@ class Tag(models.Model):
 
 class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=200)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
@@ -74,12 +84,13 @@ class Recipe(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=130)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
